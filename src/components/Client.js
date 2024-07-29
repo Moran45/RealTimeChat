a6d4028d1eb8afa99d5c3e075d9c883e6e93b6f6
Alberto Montes <dav.vazquez1719@gmail.com>
2024-07-29 13:46:28 -0600

    showUrl

--- src/components/Client.js
+++ src/components/Client.js
@@ -15,19 +15,20 @@ function Client() {
   const [showChat, setShowChat] = useState(false);
   const [unreadOwnersCount, setUnreadOwnersCount] = useState(0);
   const [intervalId, setIntervalId] = useState(null);
+  const [currentUrl] = useState(window.location.href); // Nuevo estado para almacenar la URL actual
   const ws = useWebSocket();
   const messagesEndRef = useRef(null);
   const [isDisabled, setIsDisabled] = useState(false);
 
-
   useEffect(() => {
+    console.log('Current URL:', currentUrl); // Verificar que la URL se captura correctamente
     const storedUserClient = {
       user_id: localStorage.getItem('user_id_client'),
       role: 'client', // Asumiendo que solo los admins llegan a esta página
       name: localStorage.getItem('name_client'),
       email_or_name: localStorage.getItem('name_client'),
     };
-  
+
     if (storedUserClient.user_id && storedUserClient.name) {
       setUserClient(storedUserClient);
       if (ws) {
@@ -37,14 +38,14 @@ function Client() {
       navigate('/'); // Redirigir al login si no hay información de usuario
     }
   }, [ws, navigate]);
-  
+
   useEffect(() => {
     const storedAreaId = localStorage.getItem('area_id_client');
     if (storedAreaId) {
       setSelectedArea(storedAreaId);
     }
   }, []);
-  
+
   useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -123,9 +124,7 @@ function Client() {
 
   useEffect(() => {
     const storedChatId = localStorage.getItem('chat_id_client'); // Recuperar chat_id de localStorage
-  
-    console.log('hola' + storedChatId)
-
+    console.log('hola' + storedChatId);
   }, [ws]);
 
   const startFetchingUnreadOwnersCount = () => {
@@ -148,7 +147,7 @@ function Client() {
       type: 'SELECT_AREA',
       area_id: areaId,
     }));
-  
+
     // Enviar mensaje al chat sobre la selección del área
     const message = {
       type: 'MESSAGE',
@@ -156,27 +155,31 @@ function Client() {
       chat_id: chatId,
       owner_id: userId,
       IsAdmin: 0,
-      area_id : selectedArea,
-      timestamp: new Date().toISOString()
+      area_id: selectedArea,
+      timestamp: new Date().toISOString(),
+      url: currentUrl // Incluye la URL actual en el mensaje
     };
-    
+
+    console.log('Sending message with URL:', message); // Verificar el mensaje antes de enviarlo
+
     // Enviar mensaje al servidor
     ws.send(JSON.stringify(message));
-  
+
     if (!intervalId) {
       startFetchingUnreadOwnersCount();
     }
   };
-  
+
   const handleLogin = (storedUserClient) => {
     const handleLoginResponse = (event) => {
       const msg = JSON.parse(event.data);
       if (msg.type === 'LOGIN_SUCCESS') {
         ws.send(JSON.stringify({
           type: 'GET_CHATS_CLIENT',
-          chat_id: localStorage.getItem('chat_id_client')
+          chat_id: localStorage.getItem('chat_id_client'),
+          url: currentUrl // Incluye la URL actual en la solicitud de chats
         }));
-  
+
         ws.addEventListener('message', (event) => {
           const msg = JSON.parse(event.data);
           if (msg.type === 'CHATS_CLIENT') {
@@ -185,49 +188,57 @@ function Client() {
               setSelectedArea(areaId); // Actualiza selectedArea con el valor de localStorage
               ws.send(JSON.stringify({
                 type: 'SELECT_AREA',
-                area_id: areaId
+                area_id: areaId,
+                url: currentUrl // Incluye la URL actual en la selección de área
               }));
             } else {
               console.error('No area_id found in localStorage');
             }
           }
         });
-  
+
         ws.removeEventListener('message', handleLoginResponse);
       } else if (msg.type === 'LOGIN_FAILURE') {
         navigate('/'); // Redirigir al login si el login falla
         ws.removeEventListener('message', handleLoginResponse);
       }
     };
-  
+
     ws.addEventListener('message', handleLoginResponse);
-  
-    ws.send(JSON.stringify({
+
+    const loginMessage = {
       type: 'LOGIN',
-      ...storedUserClient
-    }));
-  };  
+      ...storedUserClient,
+      url: currentUrl // Incluye la URL actual en la solicitud de login
+    };
+    console.log('Sending login message with URL:', loginMessage); // Verificar el mensaje de login
+
+    ws.send(JSON.stringify(loginMessage));
+  };
 
   const handleSendMessage = () => {
-    if (!ws || !selectedArea ) {
+    if (!ws || !selectedArea) {
       alert('No area selected or WebSocket connection not established.');
       return;
     }
-    
-    console.log(localStorage.getItem('name_client'))
-    console.log(localStorage.getItem('chat_id_client'))
+
+    console.log(localStorage.getItem('name_client'));
+    console.log(localStorage.getItem('chat_id_client'));
     // Evitar enviar el mensaje dos veces
     const message = {
       type: 'MESSAGE',
       text: messageInput,
       chat_id: chatId,
       owner_id: localStorage.getItem('user_id'),
-      area_id : selectedArea,
-      IsAdmin: 0
+      area_id: selectedArea,
+      IsAdmin: 0,
+      url: currentUrl // Incluye la URL actual en el mensaje
     };
-    ws.send(JSON.stringify(message));  //aqui es donde se envia el mensaje y se muestra en pantalla
+    console.log('Sending message with URL:', message); // Verificar el mensaje antes de enviarlo
+
+    ws.send(JSON.stringify(message)); //aqui es donde se envia el mensaje y se muestra en pantalla
     setMessageInput('');
-    
+
     if (!intervalId) {
       startFetchingUnreadOwnersCount();
     }
@@ -245,7 +256,7 @@ function Client() {
       alert('WebSocket connection not established or user_id not found.');
       return;
     }
-  
+
     // Verificar si el área ya está seleccionada
     if (selectedArea === null) {
       handleSelectArea(3, 'Reporte realizado con éxito');
@@ -257,19 +268,21 @@ function Client() {
         chat_id: chatId,
         owner_id: userId,
         IsAdmin: 0,
-        area_id : selectedArea,
-        timestamp: new Date().toISOString()
+        area_id: selectedArea,
+        timestamp: new Date().toISOString(),
+        url: currentUrl // Incluye la URL actual en el mensaje
       };
+      console.log('Sending report message with URL:', message); // Verificar el mensaje de reporte antes de enviarlo
       ws.send(JSON.stringify(message));
     }
-  
+
     setTimeout(() => {
       ws.send(JSON.stringify({
         type: 'START_CHAT',
         user_id: userId,
       }));
     }, 500);
-  
+
     try {
       const response = await fetch('https://phmsoft.tech/Ultimochatlojuro/hacer_reporte.php', {
         method: 'POST',
@@ -280,10 +293,10 @@ function Client() {
           user_id: userId,
         }),
       });
-  
+
       const text = await response.text();
       const data = JSON.parse(text);
-  
+
       if (data.error) {
         console.error('Error al hacer el reporte:', data.error);
       } else {
@@ -294,24 +307,27 @@ function Client() {
           role: 'system',
           timestamp: data.report.timestamp,
           chat_id: chatId,
-          area_id : selectedArea,
+          area_id: selectedArea,
           owner_id: userId,
+          url: currentUrl // Incluye la URL actual en el mensaje
         };
-  
+
+        console.log('Sending report message with URL:', reportMessage); // Verificar el mensaje de reporte antes de enviarlo
         ws.send(JSON.stringify({
           type: 'REPORT_MESSAGE',
           message: reportMessage,
           timestamp: data.report.timestamp,
           user_id: userId,
           chat_id: chatId,
-          area_id : selectedArea,
+          area_id: selectedArea,
           owner_id: userId,
+          url: currentUrl // Incluye la URL actual en el mensaje
         }));
       }
     } catch (error) {
       console.error('Error al ejecutar la API de hacer_reporte:', error);
     }
-  };  
+  };
 
   const toggleChat = () => {
     setShowChat(!showChat);
